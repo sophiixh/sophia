b4c8c0b2738f11275ddd961f0588c144e7f20732
sophiixh <sophia.caroline@escolar.ifrn.edu.br>
2025-07-14 11:47:19 -0300

    aula 17

--- MOTORES/motores1M/Assets/Scenes/player.cs
+++ MOTORES/motores1M/Assets/Scenes/player.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    
     public float velocidade = 40;
     public float forcaDoPulo = 4;
     
@@ -9,28 +11,34 @@ public class Player : MonoBehaviour
     
     private SpriteRenderer sprite;
     private Rigidbody2D rb;
-
-
-    void Start()
-    {
-        sprite = GetComponent<SpriteRenderer>();
-           rb = GetComponent<Rigidbody2D>();
-    }
-
     
-    void Update()
+       void Start()
+       {
+           sprite = GetComponent<SpriteRenderer>();
+           rb = GetComponent<Rigidbody2D>();
+       }
+
+       
+   void Update()
     {
         if (Input.GetKey(KeyCode.A))
-    }
-
-   gameObject.transform.position += nem Vector3(-velocidade * Tame) 
-            sprite.flipX = false; 
+        {
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime,0,0);
+            sprite.flipX = true;
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime,0,0);
+            sprite.flipX = false;
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && noChao == true)
         {
             rb.AddForce(new Vector2(0,forcaDoPulo), ForceMode2D.Impulse);
         }
+
+    }
 
     void OnCollisionEnter2D(Collision2D colisao)
     {
@@ -48,5 +56,4 @@ public class Player : MonoBehaviour
             noChao = false;
         }
     }
-
 }
